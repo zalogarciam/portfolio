0c23376f6fc4b1a7298abf66b9e938350568d771
zalogarciam <gegarciam95@gmail.com>
2023-01-31 18:04:51 -0500

    Added more social media and fix links classes

--- portfolio/src/components/common/socialMedia.jsx
+++ portfolio/src/components/common/socialMedia.jsx
@@ -4,43 +4,54 @@ import {
   BsLinkedin,
   BsGithub,
   BsTwitter,
+  BsYoutube,
 } from "react-icons/bs";
-import { FaSteam } from "react-icons/fa";
+import { FaSteam, FaTiktok } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const SocialMedia = ({}) => {
   return (
     <div>
       <div className="row pt-3">
-        <div className="col-4 d-flex d-flex--center">
+        <div className="col-3 d-flex d-flex--center">
           <Link to={"//github.com/zalogarciam"} target="_blank">
             <BsGithub className="icon icon--social-media" />
           </Link>
         </div>
 
-        <div className="col-4 d-flex d-flex--center">
+        <div className="col-3 d-flex d-flex--center">
           <Link to={"//linkedin.com/in/zalogarciam"} target="_blank">
             <BsLinkedin className="icon icon--social-media" />
           </Link>
         </div>
-        <div className="col-4 d-flex d-flex--center">
+        <div className="col-3 d-flex d-flex--center">
           <Link to={"//instagram.com/zalogarciam"} target="_blank">
             <BsInstagram className="icon icon--social-media" />
           </Link>
         </div>
+        <div className="col-3 d-flex d-flex--center">
+          <Link to={"//youtube.com/zalogarciam"} target="_blank">
+            <BsYoutube className="icon icon--social-media" />
+          </Link>
+        </div>
       </div>
       <div className="row pt-3 pb-3 d-flex d--flex-center">
-        <div className="col-4 d-flex d-flex--center">
+        <div className="col-3 d-flex d-flex--center">
           <Link to={"//twitter.com/zalogarciam"} target="_blank">
             <BsTwitter className="icon icon--social-media" />
           </Link>
         </div>
-        <div className="col-4 d-flex d-flex--center">
+        <div className="col-3 d-flex d-flex--center">
           <Link to={"//facebook.com/zalogarciam"} target="_blank">
             <BsFacebook className="icon icon--social-media" />
           </Link>
         </div>
-        <div className="col-4 d-flex d-flex--center">
+        <div className="col-3 d-flex d-flex--center">
+          <Link to={"//tiktok.com/@zalogarciam"} target="_blank">
+            <FaTiktok className="icon icon--social-media" />
+          </Link>
+        </div>
+        <div className="col-3 d-flex d-flex--center">
           <Link to={"//steamcommunity.com/id/zalogarciam/"} target="_blank">
             <FaSteam className="icon icon--social-media" />
           </Link>
