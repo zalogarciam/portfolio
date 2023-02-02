450b1b753ac57abd1d9af4138d5a1022c5092ebf
zalogarciam <gegarciam95@gmail.com>
2023-02-02 15:08:33 -0500

    Add rotation over image

--- portfolio/src/pages/aboutMe.jsx
+++ portfolio/src/pages/aboutMe.jsx
@@ -2,9 +2,9 @@ import Typewriter from "typewriter-effect";
 
 const AboutMe = () => {
   return (
-    <div>
+    <div className="container">
       <div
-        className="container text-center title-highlight-text pb-5"
+        className="text-center title-highlight-text pb-5"
         style={{ height: "12rem" }}
       >
         <Typewriter
@@ -17,57 +17,66 @@ const AboutMe = () => {
           }}
         />
       </div>
-      <div class="hero__img">
-        <img
-          src="./assets/img/zalo.webp"
-          width="256"
-          alt="Gonzalo Garcia"
-          class="hero__primary-img"
-        />
-        <figure class="hero__decorators">
-          <img
-            src="./assets/svg/html.svg"
-            alt="html"
-            width="64"
-            height="64"
-            class="hero__decorator"
-          />
-          <img
-            src="./assets/svg/css.svg"
-            alt="css"
-            width="64"
-            height="64"
-            class="hero__decorator"
-          />
-          <img
-            src="./assets/svg/js.svg"
-            alt="js"
-            width="64"
-            height="64"
-            class="hero__decorator"
-          />
-          <img
-            src="./assets/svg/react.svg"
-            alt="react"
-            width="64"
-            height="64"
-            class="hero__decorator"
-          />
-          <img
-            src="./assets/svg/csharp.svg"
-            alt="csharp"
-            width="64"
-            height="64"
-            class="section__hero-decorator"
-          />
-          <img
-            src="./assets/svg/python.svg"
-            alt="python"
-            width="64"
-            height="64"
-            class="section__hero-decorator"
-          />
-        </figure>
+      <div className="row">
+        <div className="col-sm-12 col-md-6">
+          <p className="paragraph-text">hola como estas</p>
+        </div>
+        <div className="col-sm-12 col-md-6">
+          <div className="d-flex d-flex--center">
+            <div className="rotation-container">
+              <img
+                src="../src/assets/img/zalo.webp"
+                width="256"
+                alt="Gonzalo Garcia"
+                className="rotate-img"
+              />
+              <figure className="rotation-decorators">
+                <img
+                  src="../src/assets/img/svg/html.svg"
+                  alt="html"
+                  width="64"
+                  height="64"
+                  className="rotation-decorator"
+                />
+                <img
+                  src="../src/assets/img/svg/css.svg"
+                  alt="css"
+                  width="64"
+                  height="64"
+                  className="rotation-decorator"
+                />
+                <img
+                  src="../src/assets/img/svg/js.svg"
+                  alt="js"
+                  width="64"
+                  height="64"
+                  className="rotation-decorator"
+                />
+                <img
+                  src="../src/assets/img/svg/react.svg"
+                  alt="react"
+                  width="64"
+                  height="64"
+                  className="rotation-decorator"
+                />
+                <img
+                  src="../src/assets/img/svg/csharp.svg"
+                  alt="csharp"
+                  width="64"
+                  height="64"
+                  className="rotation-decorator"
+                />
+                <img
+                  src="../src/assets/img/svg/python.svg"
+                  alt="python"
+                  width="64"
+                  height="64"
+                  className="rotation-decorator"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
