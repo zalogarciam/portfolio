1b045c83e99768818d96ba44fd223c23b6611254
zalogarciam <gegarciam95@gmail.com>
2023-02-24 12:27:10 -0500

    Updating api and minor issues

--- portfolio/src/pages/projects.jsx
+++ portfolio/src/pages/projects.jsx
@@ -34,47 +34,49 @@ const Projects = () => {
         />
       </div>
       <div className="row ">
-        {projectsData.map((project) => {
-          return (
-            <div
-              key={project.id}
-              className="col-md-12 col-sm-12 col-12 col-lg-12 col-xl-6 d-flex d-flex--center pt-3 pb-3"
-            >
-              <div className="card-container row">
-                <div className="card-header d-flex d-flex--center">
-                  <h5 className="subtitle-highlight-text pb-3 pt-3 ">
-                    {project.title}
-                  </h5>
-                </div>
-                <div className="col-md-5 col-sm-12 col-12 col-lg-5 card-img-container">
-                  <img
-                    src={project.image}
-                    className="card-img"
-                    alt={project.title}
-                  />
-                </div>
-                <div className="col-md-7 col-sm-12 col-12 col-lg-7">
-                  <div className="card-body d-flex d-flex--center">
-                    <p className="paragraph-text format-text">
-                      {project.description}
+        {[...projectsData]
+          .sort((a, b) => a.date - b.date)
+          .map((project) => {
+            return (
+              <div
+                key={project.id}
+                className="col-md-12 col-sm-12 col-12 col-lg-12 col-xl-6 d-flex d-flex--center pt-3 pb-3"
+              >
+                <div className="card-container row">
+                  <div className="card-header d-flex d-flex--center">
+                    <h5 className="subtitle-highlight-text pb-3 pt-3 ">
+                      {project.title}
+                    </h5>
+                  </div>
+                  <div className="col-md-5 col-sm-12 col-12 col-lg-5 card-img-container">
+                    <img
+                      src={project.image}
+                      className="card-img"
+                      alt={project.title}
+                    />
+                  </div>
+                  <div className="col-md-7 col-sm-12 col-12 col-lg-7">
+                    <div className="card-body d-flex d-flex--center">
+                      <p className="paragraph-text format-text">
+                        {project.description}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="card-tech-footer d-flex d-flex--center">
+                    <ProjectLinks links={project.links}></ProjectLinks>
+                  </div>
+                  <div className="card-tech-footer d-flex d-flex--center">
+                    <ProjectTechs techs={project.tech}></ProjectTechs>
+                  </div>
+                  <div className="card-footer d-flex d-flex--center">
+                    <p className="paragraph-highlight-text">
+                      Last updated {project.date}
                     </p>
                   </div>
                 </div>
-                <div className="card-tech-footer d-flex d-flex--center">
-                  <ProjectLinks links={project.links}></ProjectLinks>
-                </div>
-                <div className="card-tech-footer d-flex d-flex--center">
-                  <ProjectTechs techs={project.tech}></ProjectTechs>
-                </div>
-                <div className="card-footer d-flex d-flex--center">
-                  <p className="paragraph-highlight-text">
-                    Last updated {project.date}
-                  </p>
-                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
