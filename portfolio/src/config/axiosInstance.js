ab455537ebe5e0ec059d081311e128fdcaeeeb65
zalogarciam <gegarciam95@gmail.com>
2026-03-25 08:15:48 -0500

    Add API URL fallback to axios

--- portfolio/src/config/axiosInstance.js
+++ portfolio/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_MY_PORTFOLIO_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_MY_PORTFOLIO_API_URL || "/api/",
 });
 
 export default axiosInstance;
